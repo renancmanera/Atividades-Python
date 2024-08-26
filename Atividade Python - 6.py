21be8f9adb1ceb743babdea70d64487efcd18588
Renan Carvalho Manera <126332577+renancmanera@users.noreply.github.com>
2024-08-26 10:54:49 -0300

    Update Atividade Python - 6.py

--- Atividade Python - 6.py
+++ Atividade Python - 6.py
@@ -1,45 +1,45 @@
-Atividade Python - 6
+#Atividade Python - 6
 
-1)
+#1)
 
 somar = lambda x, y: x + y
 print(somar(3,3)) 
 
-2)
+#2)
 
 multiplicar = lambda x, y: x * y
 print(multiplicar(4,5)) 
 
-3)
+#3)
 
 comprimento = lambda : "Olá Mundo!"
 quantidade = len(comprimento())
 print(quantidade)
 
-4)
+#4)
 
 quadrado = lambda x : x **2
 print(quadrado(5))
 
-5)
+#5)
 
 numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 pares = list(filter(lambda x: x % 2 == 0, numeros))
 print(pares) 
 
-6)
+#6)
 
 cubo = lambda x : x **3
 print(cubo(3))
 
-7)
+#7)
 
 lista = [(1, 3), (4, 1), (5, 2), (2, 4)]
 
 ordenar =  sorted(lista, key=lambda ordenada: ordenada[1])
 print(ordenar) 
 
-8)
+#8)
 
 celsius_para_fahrenheit = lambda celsius: (celsius * 9/5) + 32
 
@@ -48,13 +48,13 @@ temperatura_fahrenheit = celsius_para_fahrenheit(temperatura_celsius)
 
 print(temperatura_fahrenheit)
 
-9)
+#9)
 
 nomes = ["Ana", "João", "Carlos", "Beatriz"]
 comprimentos = list(map(lambda x: len(x), nomes))
 print(comprimentos)
 
-10) 
+#10) 
 from functools import reduce
 
 numeros = [1, 2, 3, 4, 5]
